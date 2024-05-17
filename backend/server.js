7eeeb03fed51668e24eb715b73545b1041c0744f
Saqlain <itssaqlain06@gmail.com>
2024-05-17 18:55:14 +0500

    Final commit

--- backend/server.js
+++ backend/server.js
@@ -25,6 +25,7 @@ app.use("/api/users", userRoutes);
 
 app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
+// Other then upper routes the below will run
 app.get("*", (req, res) => {
 	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
